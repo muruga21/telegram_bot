172d74f561254e0e3575768289e3954d6fdd1bcb
muruga21 <murugaperumal21062004@gmail.com>
2023-05-28 21:42:14 +0530

    changed some start content

--- bot_codespace/main.py
+++ bot_codespace/main.py
@@ -3,13 +3,19 @@ from constants import API_KEY
 
 bot = telebot.TeleBot(API_KEY,parse_mode=None)
 
-@bot.message_handler(commands=["start"])
-def start(msg):
-    bot.reply_to(msg,"Hi da cibi")
-
-@bot.message_handler(commands=["cibi"])
-def start(msg):
-    bot.reply_to(msg,"cibi = motta")
+def hello(message):
+    bot.send_message(message.chat.id,"✋")
 
 
+@bot.message_handler(commands=["start","hello"])
+def start(message):
+    hello(message)
+    result = " Hey there... ✋\n Need to learn new programming language...💻\n\n dont worry I'm here to help you...\n I am a virtual bot Created to help others...🤖\n please Enter the following commands...\n\n /python - to start python  \n /c - to start c \n /c++ - to start c++ \n /java - to start java \n /HTML - to start HTMl"
+    bot.send_message(message.chat.id,result)
+
+@bot.message_handler(commands=["muruga"])
+def muruga(message):
+    bot.send_photo(message.chat.id,"https://miro.medium.com/v2/resize:fit:439/1*ZYyXvhYDGvELzYoXYpPLMg.png")
+
+@bot.message_handler(commands=["python"])
 bot.polling()
