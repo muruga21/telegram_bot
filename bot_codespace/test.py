5938d65dfebc9eaabd2f2d6c0a7b2c72ba36e38f
muruga21 <murugaperumal21062004@gmail.com>
2023-06-07 16:05:59 +0530

    completed test.py for creating json

--- bot_codespace/test.py
+++ bot_codespace/test.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-data = []
+datas = []
 
 
 links = ["https://www.w3schools.com/python/python_strings.asp","https://www.w3schools.com/python/python_strings_slicing.asp"]
@@ -22,31 +22,74 @@ titles = findtitles(links)
 print (titles)
 
 
-for url in links:
-    response = requests.get(url)
-    html_content = response.content
+def findexplanation(links):
+    explanation = []
+    for url in links:
+        response = requests.get(url)
+        html_content = response.content
 
-    soup = BeautifulSoup(html_content, features='html.parser')
-    result = soup.find_all('p')
-    lines = 0
-    explanation = ""
-    for res in result:
-        lines += 1
-        print(res.text)
-        explanation += res.text
-        if(lines==3):
-            break
-    
-print(data)
+        soup = BeautifulSoup(html_content, features='html.parser')
+        result = soup.find_all('p')
+        lines = 0
+        contentExplanation = ""
+        for res in result:
+            lines += 1
+            contentExplanation += res.text
+            if(lines==3):
+                break
+        explanation.append([contentExplanation])
+    return explanation
+
+explanations = findexplanation(links)
 
 
-# file_path = "D:/temp/data.json"
+def findsyntax(links):
+    syntax_all = []
+    for url in links:
+        response = requests.get(url)
+        html_content = response.content
+        soup = BeautifulSoup(html_content,features='html.parser')
+        result = soup.find_all(class_='w3-example')
+        lines=0
+        syntax = ''
+        for res in result:
+            lines+=1
+            syntax += res.text
+            if(lines==3):
+                break
+        syntax_all.append([syntax])
+    return syntax_all
 
-# with open(file_path, 'w') as json_file:
-#     json_data = json.dumps(data)
-#     json_file.write(json_data)
+syntaxs = findsyntax(links)
 
-# json_file.close()
+print(syntaxs)
+
+print(explanations)
+
+numberofdatas = len(explanations)
+
+def createcontent(numberofdatas,explanations):
+    for i in range(0,numberofdatas):
+        data = {
+            "image":" ",
+            "explanation":explanations[i][0],
+            "syntax": syntaxs[i][0],
+            "next":" "
+        }
+        
+        datas.append(data)
+
+createcontent(numberofdatas,explanations)
+
+print(datas)
+
+file_path = "D:/temp/data.json"
+
+with open(file_path, 'w') as json_file:
+    json_data = json.dumps(datas)
+    json_file.write(json_data)
+
+json_file.close()
 
 print("done")
 
