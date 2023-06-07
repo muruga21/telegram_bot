22378ea95d0ea09f7e385dd672903e5b51a3dbaf
muruga21 <murugaperumal21062004@gmail.com>
2023-06-07 22:22:23 +0530

    modified jsoncreater to collect datas for c.json

--- bot_codespace/jsonCreater.py
+++ bot_codespace/jsonCreater.py
@@ -6,8 +6,41 @@ import re
 datas = []
 
 
-links = ["https://www.w3schools.com/c/c_data_types.php"]
-file_path = "D:/temp/data.json"
+links = [
+    "https://www.w3schools.com/c/c_comments.php",
+    "https://www.w3schools.com/c/c_variables.php",
+    "https://www.w3schools.com/c/c_data_types.php",
+    "https://www.w3schools.com/c/c_type_conversion.php",
+    "https://www.w3schools.com/c/c_constants.php",
+    "https://www.w3schools.com/c/c_operators.php",
+    "https://www.w3schools.com/c/c_booleans.php",
+    "https://www.w3schools.com/c/c_conditions.php",
+    "https://www.w3schools.com/c/c_conditions_short_hand.php",
+    "https://www.w3schools.com/c/c_switch.php",
+    "https://www.w3schools.com/c/c_while_loop.php",
+    "https://www.w3schools.com/c/c_for_loop.php",
+    "https://www.w3schools.com/c/c_break_continue.php",
+    "https://www.w3schools.com/c/c_arrays.php",
+    "https://www.w3schools.com/c/c_arrays_multi.php",
+    "https://www.w3schools.com/c/c_strings.php",
+    "https://www.w3schools.com/c/c_strings_esc.php",
+    "https://www.w3schools.com/c/c_strings_functions.php",
+    "https://www.w3schools.com/c/c_user_input.php",
+    "https://www.w3schools.com/c/c_memory_address.php",
+    "https://www.w3schools.com/c/c_pointers.php",
+    "https://www.w3schools.com/c/c_pointers_arrays.php",
+    "https://www.w3schools.com/c/c_functions.php",
+    "https://www.w3schools.com/c/c_functions_parameters.php",
+    "https://www.w3schools.com/c/c_functions_decl.php",
+    "https://www.w3schools.com/c/c_functions_recursion.php",
+    "https://www.w3schools.com/c/c_math.php",
+    "https://www.w3schools.com/c/c_structs.php",
+    "https://www.w3schools.com/c/c_files.php",
+    "https://www.w3schools.com/c/c_files_write.php",
+    "https://www.w3schools.com/c/c_files_read.php",
+
+]
+
 
 
 def findtitles(links):
@@ -84,7 +117,7 @@ createcontent(numberofdatas,explanations)
 
 print(datas)
 
-file_path = "D:/telebot/telegram_bot/bot_codespace/data.json"
+file_path = "D:/telebot/telegram_bot/bot_codespace/c.json"
 
 with open(file_path, 'w') as json_file:
     json_data = json.dumps(datas)
