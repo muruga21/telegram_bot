1d42fc48a1e772320c89bfc823e8442c00b807f0
muruga21 <murugaperumal21062004@gmail.com>
2023-06-21 10:24:51 +0530

    corrected main.py

--- bot_codespace/main.py
+++ bot_codespace/main.py
@@ -36,7 +36,7 @@ def fetch(command):
 @bot.message_handler(commands=["start","hello"])
 def start(message):
     hello(message)
-    result = "Hey there... ✋\nNeed to learn new programming language...💻\n\ndont worry I'm here to help you...\nI am a virtual bot Created to help others...🤖\nplease Enter the following commands...\n\n/python - to start python  \n/c - to start c \n/cpp - to start c++ \n/java - to start java \n/HTML - to start HTMl\n\n/about"
+    result = "Hey there... ✋\nNeed to learn new programming language...💻\n\ndont worry I'm here to help you...\nI am a virtual bot Created to help others...🤖\nplease Enter the following commands...\n\n/python - to start python  \n/c - to start c\n\n /about"
     send(message.chat.id,result)
 
 @bot.message_handler(commands=["python"])
@@ -60,13 +60,13 @@ def python(message):
 def c(message):
     result = '''Welcome to c language\n
     C is a general-purpose programming language that was developed in the early 1970s by Dennis Ritchie at Bell Labs. C is known for its efficiency, flexibility, and low-level programming capabilities, making it suitable for system-level programming and developing applications with high performance requirements.\n\n\n
-    
+
 we have devided c language into three parts :\n
-    /c_part1\n
-    /c_part2\n
-    /c_part3\n
+    /c_part1 - To start part 1\n
+    /c_part2 - To start part 2\n
+    /c_part3 - To start part 3\n
     '''
-    send(message.chat.id,result)
+    bot.send_photo(message.chat.id,'https://contentstatic.techgig.com/photo/90325682.cms',caption=result)
 
 
 @bot.message_handler(commands=["c_part1"])
@@ -80,19 +80,22 @@ def start(message):
     /c_operators - operators in c\n
     /c_booleans - what is a booleans\n
     '''
-    send(message.chat.id,result)
+    bot.send_photo(message.chat.id,'https://images.app.goo.gl/VX5uxtezZXmT8WBr6',caption=result)
 @bot.message_handler(commands=["c_part2"])
 def start(message):
     result = '''
     Part 2 contains\n\n
-    /c_comments - to create comment lines\n
-    /c_data_types - to know about datatypes\n
-    /c_variables - How to create a variable\n
-    /c_type_conversion - type conversion in c\n
-    /c_operators - operators in c\n
-    /c_booleans - what is a booleans\n
+    /c_conditions - to know about conditional statements\n
+    /c_conditions_short_hand - what is conditions short hand\n
+    /c_switch - to know about switch statement\n
+    /c_while_loop - what is while loop\n
+    /c_for_loop - how to use for loop\n
+    /c_arrays - how to store datas in array\n
+    /c_arrays_multi - to know about multi dimension array\n
+    /c_strings - what is a string\n
+    /c_strings_functions - inbuilt string function\n
     '''
-    send(message.chat.id,result)
+    bot.send_photo(message.chat.id,'https://media.licdn.com/dms/image/C4E12AQE4VA_ZhZLp8g/article-cover_image-shrink_720_1280/0/1582135557552?e=2147483647&v=beta&t=YCMm683mzuJIiHVksVRTjmZSi7xT4ziNJEVrOfGQG3U',caption=result)
 
 # Handle incoming messages
 @bot.message_handler(func=lambda message: True)
@@ -100,16 +103,16 @@ def handle_message(message):
     messageInput = message.text
     messageInput = messageInput[1::]
     try:
-        try:
+        try: 
             if(messageInput[0]=='p'):
                 bot.send_photo(message.chat.id, python_data[messageInput]['image'],caption=fetch(messageInput))
             if(messageInput[0]=='c'):
-                bot.send_photo(message.chat.id, python_data[messageInput]['image'],caption=fetch(messageInput))
+                bot.send_photo(message.chat.id, c_data[messageInput]['image'],caption=fetch(messageInput))
         except:
             bot.send_message(message.chat.id, fetch(messageInput))
     except:
         bot.send_message(message.chat.id, "invalid command")
-    
+
 
 
 #calling bot polling function
